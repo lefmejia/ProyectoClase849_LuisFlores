4ed9278016e0e958671e9dbac05e8a76324005d7
Maria Jose Salinas <overlandwebmaster@gmail.com>
2026-06-03 17:26:38 -0600

    adaptando loginscreen al uso de screenWrapper para estilizacion basado en temas

--- MiAppMovil/src/screens/LoginScreen.tsx
+++ MiAppMovil/src/screens/LoginScreen.tsx
@@ -1,44 +1,46 @@
-import { View } from "react-native";
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
+import ScreenWrapper from "../components/ScreenWrapper";
 import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { i18n } from "../contexts/LanguageContext";
-import ScreenWrapper from "../components/ScreenWrapper";
 
-export default function LoginScreen({navigation}:any) {
-    const [email, setEmail] = useState('mjsalinas@unitec.edu');
-    const [password, setPassword] = useState('');
+export default function LoginScreen({ navigation }: any) {
+  const [email, setEmail] = useState("mjsalinas@unitec.edu");
+  const [password, setPassword] = useState("");
 
-    const {login} = useAuth();
+  const { login } = useAuth();
 
-    const handleLogin = () => {
-        try{
-            const allowed = login(email);
-            if (allowed) {
-                navigation.navigate('MainTabs');
-            }else{
-                console.log('no tiene acceso');
-            }
-        }catch (error){
-            console.log(error);
-        }
+  const handleLogin = () => {
+    try {
+      const allowed = login(email);
+      if (allowed) {
+        navigation.navigate("MainTabs");
+      } else {
+        console.log("no tiene acceso");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return(
-        <ScreenWrapper>
-            <CustomInput placeholder={"Ingresa tu correo"} 
-            value={email} 
-            onChange={setEmail}/>
+  return (
+    <ScreenWrapper>
+      <CustomInput
+        type="email"
+        placeholder="Ingresa tu correo"
+        value={email}
+        onChange={setEmail}
+      />
 
-            <CustomInput type={"password"} 
-            placeholder={"Ingresa tu contraseña"} 
-            value={password} 
-            onChange={setPassword}/>
-            
-            <CustomButton title={i18n.t('signIn')} 
-            onPress={handleLogin}/>
-        </ScreenWrapper>
+      <CustomInput
+        type="password"
+        placeholder="Ingresa tu contraseña"
+        value={password}
+        onChange={setPassword}
+      />
 
-    )
+      <CustomButton title={i18n.t("signIn")} onPress={handleLogin} />
+    </ScreenWrapper>
+  );
 }
